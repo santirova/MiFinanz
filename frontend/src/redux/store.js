e568822ff2fd8e09785e899aae9f5d7834a6e9a7
victorpahomo <vmmorales@unicauca.edu.co>
2023-10-13 00:30:03 -0500

    feat:cleaning the store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./features/themeSlice";
-//import calendarReducer from "./calendarSlice";
-//import remindersReducer from "./remindersSlice";
+
 
 export const store = configureStore({
     reducer: {
         theme: themeReducer
-       /*  calendar: calendarReducer,
-        reminders: remindersReducer, */
+
     },
 });
