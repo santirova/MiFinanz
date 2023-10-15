0f658ab19c844ab9b8c72504ac1a99d74a8dab6d
lucasb <lucasbarcelo1981@gmail.com>
2023-10-15 14:12:12 -0300

    Initial Version of LandingHeader

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,6 +2,7 @@ import ContactForm from '@/components/LandinPage/ContactForm'
 import NavBar from '@/components/LandinPage/NavBar'
 import PriceCard from '@/components/LandinPage/PriceCard'
 import TeamInfo from '@/components/LandinPage/TeamInfo'
+import LandingHeader from '../components/LandinPage/LandingHeader'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       < >
         <NavBar />
         <div className='flex flex-col items-center justify-center'>
+          <LandingHeader/>
           <PriceCard />
           <ContactForm />
           <TeamInfo />
