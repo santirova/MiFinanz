d46bb2b80fc9b45c74fa5a3ff842bf9a3b48547b
victorpahomo <vmmorales@unicauca.edu.co>
2023-10-18 15:34:52 -0500

    feat:refactoring styles

--- frontend/src/components/LandinPage/LandingHeader.jsx
+++ frontend/src/components/LandinPage/LandingHeader.jsx
@@ -6,34 +6,34 @@ import { FaWindows } from "react-icons/fa"
 export default function LandingHeader() {
   return (
     <section className="flex flex-col w-full justify-between md:flex-row">
-      <div id="leftside" className="flex flex-col w-full gap-9 justify-center items-start mt-16 md:w-1/2">
+      <div id="leftside" className="flex flex-col w-full gap-8 justify-center items-center mt-16 md:items-start md:w-1/2">
         <div className="flex flex-col items-center justify-center ">
           <h3 className="text-[28px] uppercase font-bold leading-3">
             Toma el control de tus
           </h3>
-          <h2 className="text-[52px] font-bold text-mYellow uppercase">
+          <h2 className="text-[50px] font-bold text-mYellow uppercase">
             Movimientos
           </h2>
         </div>
-        <p className="italic text-lg">
+        <p className="italic text-xl">
           Aplicación de gestión de finanzas <br /> personales que te empoderan.
         </p>
-        <button className="bg-mBlue rounded-full p-4">
-          Crear una cuenta
+        <button className="bg-mBlue text-mBlack font-medium rounded-full px-24 py-4">
+          CREAR UNA CUENTA
         </button>
         <div className="flex gap-6 items-center justify-center">
-          <div className="hidden p-1 gap-3 text-mWhite w-52 h-15 text-sm rounded-lg items-center border-solid border-2  border-white cursor-pointer md:flex">
-            <FaGooglePlay className="text-mWhite text-5xl" />
+          <div className="hidden p-2 gap-3 text-mWhite w-48 h-15 text-sm rounded-lg items-center border-solid border-2  border-white cursor-pointer md:flex">
+            <FaGooglePlay className="text-mWhite text-4xl" />
             <div className="">
-              <p className="font-semibold text-mWhite">Download on the</p>
-              <p className=" ">Google Play</p>
+              <p className="text-xs font-semibold text-mWhite">Download on the</p>
+              <p className="text-base ">Google Play</p>
             </div>
           </div>
-          <div className="hidden p-1 gap-3 text-mWhite w-52 h-15 text-sm rounded-lg items-center border-solid border-2  border-white cursor-pointer md:flex">
-            <FaWindows className="text-mWhite text-5xl" />
+          <div className="hidden p-2 gap-3 text-mWhite w-48 h-15 text-sm rounded-lg items-center border-solid border-2  border-white cursor-pointer md:flex">
+            <FaWindows className="text-mWhite text-4xl" />
             <div className="">
-              <p className="font-semibold text-mWhite">Download on the</p>
-              <p className=" ">Windows Store</p>
+              <p className="text-xs font-semibold text-mWhite">Download on the</p>
+              <p className="text-base ">Windows Store</p>
             </div>
           </div>
         </div>
