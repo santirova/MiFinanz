dbd9576803eda715caf6b7c01a9ce0a21976239b
josefinaeciolaza <josefinaecio@gmail.com>
2023-10-21 19:34:38 -0300

    comienzo order bill

--- backend/src/routes/filtersRouter.js
+++ backend/src/routes/filtersRouter.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
-const { filterEarningsHandler, filterEarningsbydataHandler, orderEarningsbydataHandler } = require('../handlers/filtersHandler');
+const { filterEarningsHandler, filterEarningsbydataHandler, orderEarningsbydataHandler, filterBillsHandler } = require('../handlers/filtersHandler');
 const filtersRouter = Router()
 
+//Filtros earning
 filtersRouter.get('/earnings',filterEarningsHandler)
 filtersRouter.get('/', filterEarningsbydataHandler)
 filtersRouter.get('/order', orderEarningsbydataHandler)
-
+//filtros bills
+filtersRouter.get('/bills/:UserId',filterBillsHandler)
 module.exports= {filtersRouter}
