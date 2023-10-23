2764a7b30ab3fa436303e793d762ed9493ff1e62
lucasb <lucasbarcelo1981@gmail.com>
2023-10-23 12:42:48 -0300

    NavHome Initial Version

---
+++ frontend/src/app/home/layout.jsx
@@ -0,0 +1,16 @@
+import NavHome from "@/components/home/navbar/NavHome";
+
+export const metadata = {
+    title: "MiFinanz",
+    description: "Home MiFinanz"
+};
+
+const LayoutHome = () => {
+  return (
+    <div className="flex">
+      <NavHome/>
+    </div>
+  )
+}
+
+export default LayoutHome
