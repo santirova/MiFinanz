3a32e7b71581cfdea704f3dbd0b1d70842be0b5e
victorpahomo <vmmorales@unicauca.edu.co>
2023-10-23 23:33:04 -0500

    refactoring code

--- frontend/src/components/LandinPage/Blog.jsx
+++ frontend/src/components/LandinPage/Blog.jsx
@@ -1,42 +1,62 @@
 import Image from "next/image";
-import blogCategory from "@/assets/landing/blogCategory.png";
-import blogFee from "@/assets/landing/blogFee.png";
-import blogGoal from "@/assets/landing/blogGoal.png";
+import { blogEntries } from "@/utils/data";
 
 export default function Blog() {
+  const leftEntry = blogEntries.find((entry) => entry?.id === 1);
+  const rightEntries = blogEntries.filter((entry) => entry?.id !== 1);
+
   return (
     <section
       id="blog"
-      className="h-[calc(100vh_-_0px)] w-full flex flex-col md:flex-row"
+      className="h-[calc(100vh_-_0px)] w-full flex flex-col items-center justify-center md:flex-row"
     >
       <div className="w-1/2">
-        <Image src={blogGoal} alt="Blog goal picture" />
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam optio
-          laudantium nostrum corrupti aut voluptate quam alias natus doloribus
-          laboriosam. Modi, magni delectus id aliquam molestiae corporis
-          provident velit voluptatum!
-        </p>
+        <Image src={leftEntry?.image} alt="Blog picture" />
+        <div key={leftEntry?.id}>
+          <h3 className="text-mRed dark:text-mYellow">
+            {leftEntry?.authorAndDate}
+          </h3>
+          <h2 className="text-mBlack dark:text-mWhite font-semibold text-2xl">
+            {leftEntry?.title}
+          </h2>
+          <p className="text-mDarkGray dark:text-mLightGray">
+            {leftEntry.desciption}
+          </p>
+          {leftEntry?.tags.map((tag) => (
+            <span
+              key={tag}
+              className="bg-yellow-700 text-mYellow px-2 text-sm rounded-xl"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
       </div>
       <div className="w-1/2">
-        <div className="flex">
-          <Image src={blogCategory} alt="Blog Category picture" />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
-            ducimus alias enim deleniti facilis minima ipsum iure perferendis
-            fuga quos. Officiis maiores est eos libero quod veniam quos ex
-            laudantium.
-          </p>
-        </div>
-        <div className="flex">
-          <Image src={blogFee} alt="Blog Fee picture" />
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
-            voluptatibus explicabo ab perferendis similique, quibusdam incidunt
-            nisi tempora neque. Optio temporibus earum repellendus voluptas
-            alias autem inventore adipisci magnam. Doloribus.
-          </p>
-        </div>
+        {rightEntries.map((entry) => (
+          <div className="flex flex-col md:flex-row">
+            <Image src={entry?.image} alt="Blog picture" />
+            <div className="" key={entry?.id}>
+              <h3 className="text-mRed dark:text-mYellow">
+                {entry?.authorAndDate}
+              </h3>
+              <h2 className="text-mBlack dark:text-mWhite font-semibold text-2xl">
+                {entry?.title}
+              </h2>
+              <p className="text-mDarkGray dark:text-mLightGray">
+                {entry.desciption}
+              </p>
+              {entry?.tags.map((tag) => (
+                <span
+                  key={tag}
+                  className="bg-yellow-700 text-mYellow px-2 text-sm rounded-xl"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,3 +1,7 @@
+import blogCategory from "@/assets/landing/blogCategory.png";
+import blogFee from "@/assets/landing/blogFee.png";
+import blogGoal from "@/assets/landing/blogGoal.png";
+
 export const navLinks = [
   {
     name: "INICIO",
@@ -18,5 +22,34 @@ export const navLinks = [
   {
     name: "SOPORTE",
     hash: "#soporte",
+  },
+];
+export const blogEntries = [
+  {
+    id: 1,
+    image: blogGoal,
+    authorAndDate: "Jhon Wick • 20 Jan 2023",
+    title: "Cómo llegar a tus metas",
+    desciption:
+      "¡Es posible! El primer paso es establecer objetivos claros y realistas. Luego, traza un plan detallado y divide tus metas en pasos manejables...",
+    tags: ["#Métodos", "#Aplicación", "#Metas"],
+  },
+  {
+    id: 2,
+    image: blogCategory,
+    authorAndDate: "Martin Gomez • 23 Nov 2023",
+    title: "Registrar categorias",
+    desciption:
+      "Las categorías de gastos son una herramienta clave en la gestión financiera personal. Estas categ...",
+    tags: ["#Métodos", "#Aplicación", "#Metas"],
+  },
+  {
+    id: 3,
+    image: blogFee,
+    authorAndDate: "Lana Swart • 18 Oct 2023",
+    title: "En cuotas",
+    desciption:
+      "Pagar en cuotas puede ser beneficioso en ciertas situaciones financieras. Aquí hay algunas circunstancias en las que puede ser útil...",
+    tags: ["#Lista", "#Tarjetas"],
   },
 ];
