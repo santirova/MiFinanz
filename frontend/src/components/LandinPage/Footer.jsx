ffed5501c566a19044b019b1433fd3196bae8954
victorpahomo <vmmorales@unicauca.edu.co>
2023-10-23 23:43:05 -0500

    refactoring code

--- frontend/src/components/LandinPage/Footer.jsx
+++ frontend/src/components/LandinPage/Footer.jsx
@@ -8,22 +8,30 @@ import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <section className="flex flex-col bg-mDarkGray w-full h-36 justify-between p-10 md:flex-row">
+    <section className="flex flex-col bg-mDarkGray w-full h-44 gap-4 md:h-36 justify-around p-10 md:flex-row">
       <div id="left-elements" className="flex gap-3 items-center">
-        <div className="bg-mWhite p-2 gap-3 text-mBlack w-48 h-15 text-sm rounded-lg items-center cursor-pointer md:flex">
+        <div className="flex bg-mWhite p-1 gap-3 text-mBlack w-44 h-15 text-sm rounded-xl items-center cursor-pointer ">
           <IoLogoGooglePlaystore className="text-mBlack text-4xl" />
           <div className="">
             <p className="text-xs font-semibold text-mBlack">Download on the</p>
             <p className="text-base ">Google Play</p>
           </div>
         </div>
-        <div className="bg-mWhite p-2 gap-3 text-mBlack w-48 h-15 text-sm rounded-lg items-center cursor-pointer md:flex">
+        <div className="flex bg-mWhite p-1 gap-3 text-mBlack w-44 h-15 text-sm rounded-xl items-center cursor-pointer ">
           <IoLogoWindows className="text-mBlack text-4xl" />
           <div className="">
             <p className="text-xs font-semibold text-mBlack">Download on the</p>
             <p className="text-base ">Windows Store</p>
           </div>
         </div>
+      </div>
+      <div
+        id="center-elements"
+        className="flex justify-center items-center gap-3"
+      >
+        <p className="text-mWhite text-sm font-semibold">
+          COPYRIGHT © 2023 miFinanz - Cohorte14
+        </p>
       </div>
       <div
         id="rigth-elements"
