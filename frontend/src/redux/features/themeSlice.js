0d9b4000c8e246a4f68c49edd5796ef31b434283
victorpahomo <vmmorales@unicauca.edu.co>
2023-10-25 23:38:02 -0500

    feat: theme on local storage

--- frontend/src/redux/features/themeSlice.js
+++ frontend/src/redux/features/themeSlice.js
@@ -1,13 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+//put election on local storage
 
+const initialState = {
+  darkMode:
+    (typeof window !== "undefined" && localStorage.getItem("darkMode")) ||
+    "dark",
+};
 export const themeSlice = createSlice({
-    name: 'theme',
-    initialState: { darkMode: 'dark' },
-    reducers: {
-        toggleTheme: (state) => {
-            state.darkMode = state.darkMode === 'dark' ? 'light' : 'dark';
-        },
+  name: "theme",
+  initialState: initialState,
+  reducers: {
+    toggleTheme: (state) => {
+      state.darkMode = state.darkMode === "dark" ? "light" : "dark";
+
+      localStorage.setItem("darkMode", state.darkMode);
     },
+  },
 });
 
 export const { toggleTheme } = themeSlice.actions;
